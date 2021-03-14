a9ed02754356001bb2d939582ff0d5cc0acd43ad
Drylozu <35281350+Drylozu@users.noreply.github.com>
2021-03-14 15:26:40 -0500

    Spelling mistakes fixed

--- src/commands/Developer/eval.ts
+++ src/commands/Developer/eval.ts
@@ -1,9 +1,8 @@
 import Command from '../../structures/Command';
-import ChildProcess from 'child_process'
+import { promisify, inspect } from 'util';
+import { exec } from 'child_process';
 import { Message } from 'discord.js';
 import Agness from '../../bot';
-import Util from 'util'
-const exec = Util.promisify(ChildProcess.exec);
 
 export default class PingCommand extends Command {
     constructor(client: Agness, category: string) {
@@ -14,28 +13,28 @@ export default class PingCommand extends Command {
         });
     }
 
-    async run(message: Message, args: string[]): Promise<void | Message> {
-        if (!args[0]) return message.channel.send('What do you evaluate?');
+    async run(message: Message, args: string[]): Promise<Message | void> {
+        if (!args[0]) return message.channel.send('What do you wanna evaluate?');
         switch (args[0].toLowerCase()) {
             case '-a': {
-                if (!args[1]) return message.channel.send('What do you evaluate?');
+                if (!args[1]) return message.channel.send('What do you wanna evaluate?');
                 try {
                     let evalued = await eval('(async() => {\n' + args.slice(1).join(' ') + '\n})();');
                     if (typeof (evalued) !== 'string') {
-                        evalued = Util.inspect(evalued, { depth: 0});
+                        evalued = inspect(evalued, { depth: 0 });
                     }
-                    message.channel.send(evalued.slice(0, 1950), { code: 'js' })
+                    message.channel.send(evalued.slice(0, 1950), { code: 'js' });
                 } catch (err) {
                     message.channel.send(err.toString().slice(0, 1950), { code: 'js' });
                 }
                 break;
             }
             case '-sh': {
-                if (!args[1]) return message.channel.send('What should I put in the terminal?');
-                let evalued = args.slice(1).join(' ')
+                if (!args[1]) return message.channel.send('What should I run in the terminal?');
+                const evalued = args.slice(1).join(' ');
                 try {
-                    const { stdout, stderr } = await exec(evalued);
-                    if (!stdout && !stderr) return message.channel.send('I executed that but there\'s no nothing to show.');
+                    const { stdout, stderr } = await promisify(exec)(evalued);
+                    if (!stdout && !stderr) return message.channel.send('I ran that but there\'s no nothing to show.');
                     if (stdout)
                         message.channel.send(stdout.slice(0, 1950), { code: 'sh' });
                     if (stderr)
@@ -48,17 +47,14 @@ export default class PingCommand extends Command {
             default: {
                 try {
                     let evalued = eval(args.join(' '));
-                    if (typeof (evalued) !== 'string') {
-                        evalued = Util.inspect(evalued, { depth: 0 });
-
-                    }
-                    message.channel.send(evalued.slice(0, 1950), { code: 'js' })
+                    if (typeof (evalued) !== 'string')
+                        evalued = inspect(evalued, { depth: 0 });
+                    message.channel.send(evalued.slice(0, 1950), { code: 'js' });
                 } catch (err) {
                     message.channel.send(err.toString().slice(0, 1950), { code: 'js' });
                 }
                 break;
             }
-
         }
     }
 }
